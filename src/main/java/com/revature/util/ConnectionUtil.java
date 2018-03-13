a408c4a483e01b92053a163b67340322a4c17a47
Brian Ethier <brianethier111@gmail.com>
2018-03-13 14:20:09 -0400

    repositories started

---
+++ src/main/java/com/revature/util/ConnectionUtil.java
@@ -0,0 +1,20 @@
+package com.revature.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import org.apache.log4j.Logger;
+
+public class ConnectionUtil {
+
+	
+	private static Logger logger = Logger.getLogger(ConnectionUtil.class);
+
+	public static Connection getConnection() throws SQLException{
+		String url = "jdbc:oracle:thin:@revaturebankdb.cxcscohikus1.us-east-2.rds.amazonaws.com:1521:ORCL";
+		String username = "REIMBURSEMENT_DB";
+		String password = "p4ssw0rd";
+		return DriverManager.getConnection(url, username, password);
+	}
+}
