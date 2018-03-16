1848803e1ed7149ac58476587069119bcec89312
Brian Ethier <brianethier111@gmail.com>
2018-03-16 19:25:57 -0400

    repo layer finally finished after losing all previous commits because I still don't understand git bash

--- src/main/java/com/revature/repository/EmployeeRepositoryJdbc.java
+++ src/main/java/com/revature/repository/EmployeeRepositoryJdbc.java
@@ -1,64 +1,254 @@
 package com.revature.repository;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashSet;
 import java.util.Set;
 
+import org.apache.log4j.Logger;
+
 import com.revature.model.Employee;
+import com.revature.model.EmployeeRole;
 import com.revature.model.EmployeeToken;
+import com.revature.util.ConnectionUtil;
 
 public class EmployeeRepositoryJdbc implements EmployeeRepository {
 
+	private static final Logger logger = Logger.getLogger(EmployeeRepositoryJdbc.class);
+
+	private static EmployeeRepositoryJdbc repository = new EmployeeRepositoryJdbc();
+
+	private EmployeeRepositoryJdbc()
+	{
+	}
+
+	public static EmployeeRepositoryJdbc getInstance()
+	{
+		return repository;
+	}
+
 	@Override
-	public boolean insert(Employee employee) {
+	public boolean insert(Employee employee)
+	{
+		try (Connection connection = ConnectionUtil.getConnection())
+		{
+			final String command = "INSERT INTO USER_T VALUES(NULL,?,?,?,?,?,?)";
+			PreparedStatement statement = connection.prepareStatement(command);
+			int statementIndex = 0;
+
+			statement.setString(++statementIndex, employee.getFirstName().toUpperCase());
+			statement.setString(++statementIndex, employee.getLastName().toUpperCase());
+			statement.setString(++statementIndex, employee.getUsername().toLowerCase());
+			statement.setString(++statementIndex, employee.getPassword());
+			statement.setString(++statementIndex, employee.getEmail().toLowerCase());
+			statement.setInt(++statementIndex, employee.getEmployeeRole().getId());
+
+			if ( statement.executeUpdate() != 0 )
+			{
+				logger.trace("inserted");
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+
+		}
+		catch (SQLException e)
+		{
+			logger.error("user/employee insertion failed", e);
+		}
+		return false;
+	}
+
+	@Override
+	public boolean update(Employee employee)
+	{
+		try (Connection connection = ConnectionUtil.getConnection())
+		{
+			final String command = "UPDATE USER_T SET U_FIRSTNAME = ?, U_LASTNAME = ? , U_PASSWORD = ?, U_EMAIL = ?, UR_ID = ? WHERE U_ID =  ?";
+			PreparedStatement statement = connection.prepareStatement(command);
+			int statementIndex = 0;
+
+			statement.setString(++statementIndex, employee.getFirstName().toUpperCase());
+			statement.setString(++statementIndex, employee.getLastName().toUpperCase());
+			statement.setString(++statementIndex, employee.getPassword());
+			statement.setString(++statementIndex, employee.getEmail().toLowerCase());
+			statement.setInt(++statementIndex, employee.getEmployeeRole().getId());
+			statement.setInt(++statementIndex, employee.getId());
+
+			if ( statement.executeUpdate() != 0 )
+			{
+				logger.trace("Updated employee");
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+		catch (SQLException e)
+		{
+			logger.error("Employee failed to update", e);
+		}
+		return false;
+	}
+
+	@Override
+	public Employee select(int employeeId)
+	{
+		try (Connection connection = ConnectionUtil.getConnection())
+		{
+			final String command = "SELECT U.U_ID, U.U_FIRSTNAME, U.U_LASTNAME, U.U_USERNAME, U.U_PASSWORD, U.U_EMAIL, U.UR_ID, UR.UR_TYPE FROM USER_T U INNER JOIN USER_ROLE UR ON U.UR_ID = UR.UR_ID WHERE U.U_ID = ?";
+			PreparedStatement statement = connection.prepareStatement(command);
+
+			statement.setInt(1, employeeId);
+
+			ResultSet resultSet = statement.executeQuery();
+
+			while (resultSet.next())
+			{
+				Employee employee = new Employee();
+
+				employee.setId(resultSet.getInt("U_ID"));
+				employee.setFirstName(resultSet.getString("U_FIRSTNAME"));
+				employee.setLastName(resultSet.getString("U_LASTNAME"));
+				employee.setUsername(resultSet.getString("U_USERNAME"));
+				employee.setPassword(resultSet.getString("U_PASSWORD"));
+				employee.setEmail(resultSet.getString("U_EMAIL"));
+				employee.setEmployeeRole(new EmployeeRole(resultSet.getInt("UR_ID"), resultSet.getString("UR_TYPE")));
+
+				return employee;
+			}
+		}
+		catch (SQLException e)
+		{
+			logger.error("Specified Employee not found (ID)", e);
+		}
+		return new Employee();
+	}
+
+	@Override
+	public Employee select(String username)
+	{
+		try (Connection connection = ConnectionUtil.getConnection())
+		{
+			final String command = "SELECT U.U_ID, U.U_FIRSTNAME, U.U_LASTNAME, U.U_USERNAME, U.U_PASSWORD, U.U_EMAIL, U.UR_ID, UR.UR_TYPE FROM USER_T U INNER JOIN USER_ROLE UR ON U.UR_ID = UR.UR_ID WHERE U.U_USERNAME = ?";
+			PreparedStatement statement = connection.prepareStatement(command);
+
+			statement.setString(1, username);
+
+			ResultSet resultSet = statement.executeQuery();
+
+			while (resultSet.next())
+			{
+				Employee employee = new Employee();
+
+				employee.setId(resultSet.getInt("U_ID"));
+				employee.setFirstName(resultSet.getString("U_FIRSTNAME"));
+				employee.setLastName(resultSet.getString("U_LASTNAME"));
+				employee.setUsername(resultSet.getString("U_USERNAME"));
+				employee.setPassword(resultSet.getString("U_PASSWORD"));
+				employee.setEmail(resultSet.getString("U_EMAIL"));
+				employee.setEmployeeRole(new EmployeeRole(resultSet.getInt("UR_ID"), resultSet.getString("UR_TYPE")));
+
+				return employee;
+			}
+		}
+		catch (SQLException e)
+		{
+			logger.error("Specified Employee not found (username)", e);
+		}
+		return new Employee();
+	}
+
+	@Override
+	public Set<Employee> selectAll()
+	{
+		Set<Employee> employees = new HashSet<>();
+
+		try (Connection connection = ConnectionUtil.getConnection())
+		{
+			final String command = "SELECT U.U_ID, U.U_FIRSTNAME, U.U_LASTNAME, U.U_USERNAME, U.U_PASSWORD, U.U_EMAIL, U.UR_ID, UR.UR_TYPE FROM USER_T U INNER JOIN USER_ROLE UR ON U.UR_ID = UR.UR_ID";
+			PreparedStatement statement = connection.prepareStatement(command);
+
+			ResultSet resultSet = statement.executeQuery();
+
+			while (resultSet.next())
+			{
+				Employee employee = new Employee();
+
+				employee.setId(resultSet.getInt("U_ID"));
+				employee.setFirstName(resultSet.getString("U_FIRSTNAME"));
+				employee.setLastName(resultSet.getString("U_LASTNAME"));
+				employee.setUsername(resultSet.getString("U_USERNAME"));
+				employee.setPassword(resultSet.getString("U_PASSWORD"));
+				employee.setEmail(resultSet.getString("U_EMAIL"));
+				employee.setEmployeeRole(new EmployeeRole(resultSet.getInt("UR_ID"), resultSet.getString("UR_TYPE")));
+
+				employees.add(employee);
+			}
+		}
+		catch (SQLException e)
+		{
+			logger.error("Select all employees failed.", e);
+		}
+		return employees;
+	}
+
+	@Override
+	public String getPasswordHash(Employee employee)
+	{
+		try(Connection connection = ConnectionUtil.getConnection()) {
+
+			int statementIndex = 0;
+
+			String command = "SELECT GET_HASH(?) AS HASH FROM DUAL";
+
+			PreparedStatement statement = connection.prepareStatement(command);
+
+			statement.setString(++statementIndex, employee.getPassword());
+
+			ResultSet result = statement.executeQuery();
+
+
+
+			if(result.next()) {
+
+				return result.getString("HASH");
+
+			}
+
+		} catch (SQLException e) {
+
+			logger.warn("Exception getting customer hash", e);
+
+		} 
+
+		return new String();	
+	}
+
+	@Override
+	public boolean insertEmployeeToken(EmployeeToken employeeToken)
+	{
 		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
-	public boolean update(Employee employee) {
+	public boolean deleteEmployeeToken(EmployeeToken employeeToken)
+	{
 		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
-	public Employee select(int employeeId) {
+	public EmployeeToken selectEmployeeToken(EmployeeToken employeeToken)
+	{
 		// TODO Auto-generated method stub
 		return null;
 	}
-
-	@Override
-	public Employee select(String username) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Set<Employee> selectAll() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public String getPasswordHash(Employee employee) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public boolean insertEmployeeToken(EmployeeToken employeeToken) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean deleteEmployeeToken(EmployeeToken employeeToken) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public EmployeeToken selectEmployeeToken(EmployeeToken employeeToken) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
 }
+
--- src/main/java/com/revature/repository/ReimbursementRepositoryJdbc.java
+++ src/main/java/com/revature/repository/ReimbursementRepositoryJdbc.java
@@ -1,58 +1,335 @@
 package com.revature.repository;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.HashSet;
 import java.util.Set;
 
+import org.apache.log4j.Logger;
+
 import com.revature.model.Reimbursement;
+import com.revature.model.ReimbursementStatus;
 import com.revature.model.ReimbursementType;
+import com.revature.util.ConnectionUtil;
+
+
+//table: reimbursement
+//R_ID          NOT NULL NUMBER         
+//R_REQUESTED   NOT NULL TIMESTAMP(6)   
+//R_RESOLVED             TIMESTAMP(6)   
+//R_AMOUNT      NOT NULL NUMBER(8,2)    
+//R_DESCRIPTION          VARCHAR2(4000) 
+//R_RECEIPT              BLOB           
+//EMPLOYEE_ID   NOT NULL NUMBER         
+//MANAGER_ID             NUMBER         
+//RS_ID         NOT NULL NUMBER         
+//RT_ID         NOT NULL NUMBER   
+//
 
 public class ReimbursementRepositoryJdbc implements ReimbursementRepository {
 
+
+	private static Logger logger = Logger.getLogger(ReimbursementRepositoryJdbc.class);
+
 	@Override
 	public boolean insert(Reimbursement reimbursement) {
-		// TODO Auto-generated method stub
+		try(Connection connection = ConnectionUtil.getConnection()) {
+			int statementIndex = 0;
+			String command = "INSERT INTO REIMBURSEMENT VALUES(NULL,?,?,?,?,NULL,?,?,?,?)";
+
+			PreparedStatement statement = connection.prepareStatement(command);
+
+			//Set attributes to be inserted
+			statement.setTimestamp(++statementIndex, Timestamp.valueOf(reimbursement.getRequested()));
+			statement.setTimestamp(++statementIndex, Timestamp.valueOf(reimbursement.getResolved()));
+			statement.setDouble(++statementIndex, reimbursement.getAmount());
+			statement.setString(++statementIndex, reimbursement.getDescription());
+			//statement.setBlob(++statementIndex, (Blob)reimbursement.getReceipt());
+			statement.setInt(++statementIndex, reimbursement.getRequester().getId());
+			statement.setInt(++statementIndex, reimbursement.getApprover().getId());
+			statement.setInt(++statementIndex, reimbursement.getStatus().getId());
+			statement.setInt(++statementIndex, reimbursement.getType().getId());
+
+			if(statement.executeUpdate() > 0) {
+				return true;
+			}
+		} catch (SQLException e) {
+			logger.warn("Exception creating a new employee", e);
+		}
 		return false;
 	}
 
 	@Override
 	public boolean update(Reimbursement reimbursement) {
-		// TODO Auto-generated method stub
+		try(Connection connection = ConnectionUtil.getConnection()){
+			int statementIndex = 0;
+
+			String command="UPDATE REIMBURSEMENT SET MANAGER_ID = ?, RS_ID = ?   WHERE R_ID = ?";
+
+			logger.trace("getting statement object in update account");
+
+			PreparedStatement statement = connection.prepareStatement(command);
+			//Set attributes to be inserted
+			//statement.setTimestamp(++statementIndex, Timestamp.valueOf(reimbursement.getRequested()));
+			//statement.setTimestamp(++statementIndex, Timestamp.valueOf(reimbursement.getResolved()));
+			//statement.setDouble(++statementIndex, reimbursement.getAmount());
+			//statement.setString(++statementIndex, reimbursement.getDescription());
+			//statement.setBlob(++statementIndex, (Blob)reimbursement.getReceipt());
+			//statement.setInt(++statementIndex, reimbursement.getRequester().getId());
+
+			//change to get logged user id.
+			statement.setInt(++statementIndex, reimbursement.getApprover().getId());
+
+			//change to enum/static of status value
+			statement.setInt(++statementIndex, reimbursement.getStatus().getId());
+			//statement.setInt(++statementIndex, reimbursement.getType().getId());
+			logger.trace("parameters for update of account set");
+			//System.out.println(statement.toString());
+
+
+			if(statement.executeUpdate() > 0){
+				logger.trace("account updated succefully");
+				return true;
+			}
+		} catch (SQLException e) {
+			logger.error("exception thrown while updating ", e);
+		}
+
+
 		return false;
 	}
 
 	@Override
 	public Reimbursement select(int reimbursementId) {
-		// TODO Auto-generated method stub
+		logger.trace("Selecting reimbursement");
+		try (Connection connection = ConnectionUtil.getConnection()) {
+			int parameterIndex = 0;
+			String command = "SELECT * FROM REIMBURSEMENT R INNER JOIN REIMBURSEMENT_STATUS RS ON R.RS_ID = RS.RS_ID "
+					+ "INNER JOIN REIMBURSEMENT_TYPE RT ON R.RT_ID = RT.RT_ID WHERE R.R_ID = ?";  
+
+			PreparedStatement statement = connection.prepareStatement(command);
+			statement.setInt(++parameterIndex, reimbursementId);
+
+			ResultSet result = statement.executeQuery();
+
+			if (result.next()) {
+				Reimbursement reimbursement = new Reimbursement(
+						result.getInt("R_ID"),
+						result.getTimestamp("R_REQUESTED").toLocalDateTime(),
+						null,
+						result.getDouble("R_AMOUNT"),
+						result.getString("R_DESCRIPTION"),
+						EmployeeRepositoryJdbc.getInstance().select(result.getInt("EMPLOYEE_ID")),
+						EmployeeRepositoryJdbc.getInstance().select(result.getInt("MANAGER_ID")),
+						new ReimbursementStatus(
+								result.getInt("RS_ID"),
+								result.getString("RS_STATUS")
+								),
+						new ReimbursementType(
+								result.getInt("RT_ID"),
+								result.getString("RT_TYPE")
+								)
+						);
+				if (result.getString("R_RESOLVED") != null) {
+					reimbursement.setResolved(result.getTimestamp("R_RESOLVED").toLocalDateTime());
+				}
+				return reimbursement;
+			}
+		} catch (SQLException e) {
+			logger.error("Exception thrown while selecting reimbursement", e);
+		}
 		return null;
 	}
 
 	@Override
 	public Set<Reimbursement> selectPending(int employeeId) {
-		// TODO Auto-generated method stub
+		logger.trace("Selecting pending reimbursement");
+		try (Connection connection = ConnectionUtil.getConnection()) {
+			int parameterIndex = 0;
+			String command = "SELECT * FROM REIMBURSEMENT R INNER JOIN REIMBURSEMENT_STATUS RS ON R.RS_ID = RS.RS_ID "
+					+ "INNER JOIN REIMBURSEMENT_TYPE RT ON R.RT_ID = RT.RT_ID WHERE R.EMPLOYEE_ID = ? AND R.RS_ID = ?";
+
+			PreparedStatement statement = connection.prepareStatement(command);
+			statement.setInt(++parameterIndex, employeeId);
+			statement.setInt(++parameterIndex, 1);
+
+			ResultSet result = statement.executeQuery();
+			Set<Reimbursement> reimbursements = new HashSet<>();
+
+			while (result.next()) {
+				reimbursements.add(new Reimbursement(
+						result.getInt("R_ID"),
+						result.getTimestamp("R_REQUESTED").toLocalDateTime(),
+						null,
+						result.getDouble("R_AMOUNT"),
+						result.getString("R_DESCRIPTION"),
+						EmployeeRepositoryJdbc.getInstance().select(result.getInt("EMPLOYEE_ID")),
+						EmployeeRepositoryJdbc.getInstance().select(result.getInt("MANAGER_ID")),
+						new ReimbursementStatus(
+								result.getInt("RS_ID"),
+								result.getString("RS_STATUS")
+								),
+						new ReimbursementType(
+								result.getInt("RT_ID"),
+								result.getString("RT_TYPE")
+								)
+						));
+			}
+			return reimbursements;
+
+		} catch (SQLException e) {
+			logger.error("Exception thrown while selecting pending reimbursement", e);
+		}
 		return null;
 	}
 
 	@Override
 	public Set<Reimbursement> selectFinalized(int employeeId) {
-		// TODO Auto-generated method stub
+		logger.trace("Selecting finalized reimbursement");
+		try (Connection connection = ConnectionUtil.getConnection()) {
+			int parameterIndex = 0;
+			String command = "SELECT * FROM REIMBURSEMENT R INNER JOIN REIMBURSEMENT_STATUS RS ON R.RS_ID = RS.RS_ID "
+					+ "INNER JOIN REIMBURSEMENT_TYPE RT ON R.RT_ID = RT.RT_ID WHERE R.EMPLOYEE_ID = ? AND (R.RS_ID = ? OR R.RS_ID = ?)";
+
+			PreparedStatement statement = connection.prepareStatement(command);
+			statement.setInt(++parameterIndex, employeeId);
+			statement.setInt(++parameterIndex, 2);
+			statement.setInt(++parameterIndex, 3);
+
+			ResultSet result = statement.executeQuery();
+			Set<Reimbursement> reimbursements = new HashSet<>();
+
+			while (result.next()) {
+				reimbursements.add(new Reimbursement(
+						result.getInt("R_ID"),
+						result.getTimestamp("R_REQUESTED").toLocalDateTime(),
+						null,
+						result.getDouble("R_AMOUNT"),
+						result.getString("R_DESCRIPTION"),
+						EmployeeRepositoryJdbc.getInstance().select(result.getInt("EMPLOYEE_ID")),
+						EmployeeRepositoryJdbc.getInstance().select(result.getInt("MANAGER_ID")),
+						new ReimbursementStatus(
+								result.getInt("RS_ID"),
+								result.getString("RS_STATUS")
+								),
+						new ReimbursementType(
+								result.getInt("RT_ID"),
+								result.getString("RT_TYPE")
+								)
+						));
+			}
+			return reimbursements;
+		} catch (SQLException e) {
+			logger.error("Exception thrown while selecting finalized reimbursement", e);
+		}
 		return null;
 	}
 
 	@Override
 	public Set<Reimbursement> selectAllPending() {
-		// TODO Auto-generated method stub
+		logger.trace("Selecting all pending reimbursement");
+		try (Connection connection = ConnectionUtil.getConnection()) {
+			int parameterIndex = 0;
+			String command = "SELECT * FROM REIMBURSEMENT R INNER JOIN REIMBURSEMENT_STATUS RS ON R.RS_ID = RS.RS_ID "
+					+ "INNER JOIN REIMBURSEMENT_TYPE RT ON R.RT_ID = RT.RT_ID WHERE R.RS_ID = ?";
+
+			PreparedStatement statement = connection.prepareStatement(command);
+			statement.setInt(++parameterIndex, 1);
+
+			ResultSet result = statement.executeQuery();
+			Set<Reimbursement> reimbursements = new HashSet<>();
+
+			while (result.next()) {
+				reimbursements.add(new Reimbursement(
+						result.getInt("R_ID"),
+						result.getTimestamp("R_REQUESTED").toLocalDateTime(),
+						null,
+						result.getDouble("R_AMOUNT"),
+						result.getString("R_DESCRIPTION"),
+						EmployeeRepositoryJdbc.getInstance().select(result.getInt("EMPLOYEE_ID")),
+						EmployeeRepositoryJdbc.getInstance().select(result.getInt("MANAGER_ID")),
+						new ReimbursementStatus(
+								result.getInt("RS_ID"),
+								result.getString("RS_STATUS")
+								),
+						new ReimbursementType(
+								result.getInt("RT_ID"),
+								result.getString("RT_TYPE")
+								)
+						));
+			}
+			return reimbursements;
+		} catch (SQLException e) {
+			logger.error("Exception thrown while selecting pending reimbursement", e);
+		}
 		return null;
 	}
 
 	@Override
 	public Set<Reimbursement> selectAllFinalized() {
-		// TODO Auto-generated method stub
+		logger.trace("Selecting all finalized reimbursement");
+		try (Connection connection = ConnectionUtil.getConnection()) {
+			int parameterIndex = 0;
+			String command = "SELECT * FROM REIMBURSEMENT R INNER JOIN REIMBURSEMENT_STATUS RS ON R.RS_ID = RS.RS_ID "
+					+ "INNER JOIN REIMBURSEMENT_TYPE RT ON R.RT_ID = RT.RT_ID WHERE R.RS_ID = ? OR R.RS_ID = ?";
+
+			PreparedStatement statement = connection.prepareStatement(command);
+			statement.setInt(++parameterIndex, 2);
+			statement.setInt(++parameterIndex, 3);
+
+			ResultSet result = statement.executeQuery();
+			Set<Reimbursement> reimbursements = new HashSet<>();
+
+			while (result.next()) {
+				reimbursements.add(new Reimbursement(
+						result.getInt("R_ID"),
+						result.getTimestamp("R_REQUESTED").toLocalDateTime(),
+						null,
+						result.getDouble("R_AMOUNT"),
+						result.getString("R_DESCRIPTION"),
+						EmployeeRepositoryJdbc.getInstance().select(result.getInt("EMPLOYEE_ID")),
+						EmployeeRepositoryJdbc.getInstance().select(result.getInt("MANAGER_ID")),
+						new ReimbursementStatus(
+								result.getInt("RS_ID"),
+								result.getString("RS_STATUS")
+								),
+						new ReimbursementType(
+								result.getInt("RT_ID"),
+								result.getString("RT_TYPE")
+								)
+						));
+			}
+			return reimbursements;
+		} catch (SQLException e) {
+			logger.error("Exception thrown while selecting finalized reimbursement", e);
+		}
 		return null;
 	}
 
 	@Override
 	public Set<ReimbursementType> selectTypes() {
-		// TODO Auto-generated method stub
+		logger.trace("Selecting types");
+		try (Connection connection = ConnectionUtil.getConnection()) {
+			String command = "SELECT * FROM REIMBURSEMENT_TYPE";
+
+			PreparedStatement statement = connection.prepareStatement(command);
+
+			ResultSet result = statement.executeQuery();
+			Set<ReimbursementType> types = new HashSet<>();
+
+			while (result.next()) {
+				types.add(new ReimbursementType(
+						result.getInt("RT_ID"),
+						result.getString("RT_TYPE")
+						));
+			}
+			return types;
+		} catch (SQLException e) {
+			logger.error("Exception thrown while selecting types", e);
+		}
 		return null;
 	}
-
 }
